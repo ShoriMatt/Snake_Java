1403d26e5c63e80c4728d1a7b040fc3a6f67d6bd
rmatthia <matthias.robbe@ynov.com>
2026-05-11 12:22:59 +0200

    third commit

--- src/Snake.java
+++ src/Snake.java
@@ -1,7 +1,18 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class Snake {
     public char snakeHead = '@';
     public char snakeBody = 'O';
+    public int snakeSize = 0;
+    Random r = new Random();
+    
+    public ArrayList<Integer> snakeSpawn(int X, int Y) {
+        int y = r.nextInt(3, Y - 2);
+        int x = r.nextInt(3, X - 2);
+        ArrayList<Integer> rep = new ArrayList<Integer>();
+        return rep ;
+    }
 
-    public void snakeSpawn() {
-        x = nextInt(
+    
 }
--- src/Structures.java
+++ src/Structures.java
@@ -1,18 +1,18 @@
 public class Structures {
     private char wall = '#';
 
-    public int x;
-    public int y;
+    public int X = 16;
+    public int Y = 40;
 
-    public char[][] stage = new char[16][40];
+    public char[][] stage = new char[X][Y];
 
     public void createWall(){
-        for (int y = 0; y < this.stage.length; y++) {
-            for (int x = 0; x < this.stage[y].length; x++) {
-                if (y == 0 || y == this.stage.length - 1 || x == 0 || x == this.stage[y].length - 1) {
-                    this.stage[y][x] = wall;
+        for (int y = 0; y < stage.length; y++) {
+            for (int x = 0; x < stage[y].length; x++) {
+                if (y == 0 || y == stage.length - 1 || x == 0 || x == stage[y].length - 1) {
+                    stage[y][x] = wall;
                 } else {
-                    this.stage[y][x] = ' ';
+                    stage[y][x] = ' ';
                 }
             }
         }
@@ -24,6 +24,29 @@ public class Structures {
                 System.out.print(stage[y][x]);
             }
             System.out.println();
+        }
+    }
+
+    public ResultRandomPos genRandomPos(boolean isSnake) {
+        int y = (int) Math.round(Math.random() * this.Y - 2) + 1;
+        int x = (int) Math.round(Math.random() * this.X - 2) + 1;
+
+        if (isSnake) {
+            if (x + 3 > this.Y - 1) {
+                genRandomPos(isSnake);
+            }
+            return new ResultRandomPos(x, y);
+        }
+        return new ResultRandomPos(x, y);
+    }
+
+    final class ResultRandomPos {
+        int x;
+        int y;
+
+        public ResultRandomPos(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
